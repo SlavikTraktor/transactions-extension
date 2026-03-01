5b6cc6dc45b52e19622d2a44a8ed0a1cacdb2cd8
Sviatoslav Sobakin <slavasobb@gmail.com>
2026-03-01 20:29:54 +0400

    paypal fetch

--- src/content/fetchTransactions/fetchPaypalTransactions.ts
+++ src/content/fetchTransactions/fetchPaypalTransactions.ts
@@ -1,14 +1,50 @@
-import type { FetchTransactionsFunction, FetchTransactionsResult } from "../types";
+import { format } from "date-fns";
+import type {
+  FetchTransactionsFunction,
+  FetchTransactionsResult,
+} from "../types";
 // import { format, getTime } from "date-fns";
 
 export const PAYPAL_TRANSACTIONS_URL =
-  "https://www.tbank.ru/api/common/v1/operations?appName=supreme&appVersion=0.0.1&origin=web%2Cib5%2Cplatform&sessionid={sessionId}&start={start}&end={end}"; // start 1769457600000 end 1772210482704
+  "https://www.paypal.com/myaccount/activities/filter?q={q}"; // start 1769457600000 end 1772210482704
 
+const baseQuery =
+  "free_text_search=&start_date={start}&end_date={end}&type=&status=&currency=&filter_id=&issuance_product_name=&asset_names=&asset_symbols=";
+// 2025-12-02 to 2026-03-01
 export const fetchPaypalTransactions: FetchTransactionsFunction = async (
   dateFrom: Date,
   dateTo: Date,
   limit = 1000,
 ): Promise<FetchTransactionsResult[]> => {
+  const encodedQuery = btoa(
+    baseQuery
+      .replace("{start}", format(dateFrom, "yyyy-MM-dd"))
+      .replace("{end}", format(dateTo, "yyyy-MM-dd")),
+  );
 
-  return [];
+  const resUrl = PAYPAL_TRANSACTIONS_URL.replace("{q}", encodedQuery);
+
+  const res = await fetch(resUrl, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((res) => {
+      return res.data.data.activity.transactions.map((operation: any) => ({
+        uuid: `${operation.id}`,
+        timestamp: format(
+          new Date(operation.date.rawDate.date),
+          "yyyy-MM-dd HH:mm:ss",
+        ),
+        amount: +operation.amountInfo.contents.amountText
+          .replace("&minus;", "-")
+          .replace(/[^0-9,+-]/g, "")
+          .replace(",", "."),
+        description:
+          operation.descriptionInfo.content.transactionDescriptionText,
+        sender: operation.counterpartyInfo.counterpartyName,
+        currency: operation.amounts.currencyCode,
+        source_type: "paypal",
+      }));
+    });
+  return res;
 };
